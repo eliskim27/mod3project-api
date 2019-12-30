15b0d78e86cb41d8990824b4a5b981918c174d75
eliskim27 <eliskim27@gmail.com>
2019-12-30 10:40:10 -0500

    first commit

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,14 @@
+class Api::V1::UsersController < ApplicationController
+
+    # def index
+        # pass in any collections we want (users = User.all)
+        # render json: users, except: [:created_at, :updated_at]
+        # can also use 'only:' and 'include:'
+    # end
+
+# # still need to do:
+# private
+#     user_params
+#         params.require(:user).permit(:'attributes')
+#     end
+end
